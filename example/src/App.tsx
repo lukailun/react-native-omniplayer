5cde0a3aa53982b9e83a2301d8d30c8dc73aedbe
Karen <lukailun@mampod.com>
2024-11-20 11:26:47 +0800

    feat: add onEnd and onError events

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,24 +1,20 @@
 import { StyleSheet, View } from 'react-native';
-import { VideoPlayer, PlayerMode, ResizeMode } from 'react-native-omniplayer';
+import { VideoPlayer, ResizeMode } from 'react-native-omniplayer';
 
 export default function App() {
-  const sampleVideoUri =
+  const mp4VideoUri =
     'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
   return (
     <View style={styles.container}>
       <VideoPlayer
-        mode={PlayerMode.VLC}
+        style={styles.video}
         resizeMode={ResizeMode.CONTAIN}
-        uri={sampleVideoUri}
-        onLoad={(event) => {
-          console.log('onLoad: ', event);
-        }}
-        // onProgress={(event) => {
-        //   console.log('onProgress: ', event);
-        // }}
-        onBuffer={(event) => {
-          console.log('onBuffer: ', event);
-        }}
+        uri={mp4VideoUri}
+        onLoad={(event) => console.log('onLoad: ', event)}
+        onProgress={(event) => console.log('onProgress: ', event)}
+        onBuffer={(event) => console.log('onBuffer: ', event)}
+        onEnd={() => console.log('onEnd')}
+        onError={(event) => console.log('onError: ', event)}
       />
     </View>
   );
@@ -29,5 +25,8 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  video: {
+    height: '50%',
   },
 });
--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,5 +1,10 @@
-import { forwardRef, useRef } from 'react';
-import { StyleSheet, Text, type StyleProp, type ViewStyle } from 'react-native';
+import { useRef } from 'react';
+import {
+  StyleSheet,
+  Platform,
+  type StyleProp,
+  type ViewStyle,
+} from 'react-native';
 import Video, {
   ResizeMode,
   type VideoRef,
@@ -9,9 +14,6 @@ import { VLCPlayer } from 'react-native-vlc-media-player';
 import PlayerMode from '../types/PlayerMode';
 
 export interface VideoPlayerRef {
-  setUrl?: (url: string) => void;
-  videoOnSeek?: () => void;
-  videoOnSlidingComplete?: (value: number) => void;
   play?: () => void;
   pause?: () => void;
 }
@@ -36,124 +38,133 @@ export interface VideoPlayerProps {
     remainingTime: number;
   }) => void;
   onBuffer?: (event: { isBuffering: boolean }) => void;
-  onTogglePlayStatus?: (isPlaying: boolean) => void;
-  onToggleShowsMask?: (showsMask: boolean) => void;
-  onToggleSubtitles?: (showsSubtitles: boolean) => void;
-  onClose?: () => void;
-  onPictureInPicture?: () => void;
-  onDetails?: () => void;
-  onFullscreen?: () => void;
+  onEnd?: () => void;
+  onError?: (event: { error: string | undefined }) => void;
 }
 
-const VideoPlayer = forwardRef<VideoPlayerRef, VideoPlayerProps>(
-  ({
-    mode = PlayerMode.AUTO,
-    uri,
-    style,
-    controls,
-    muted,
-    paused,
-    playInBackground,
-    rate,
-    repeat,
-    resizeMode,
-    volume,
-    onLoad,
-    onProgress,
-    onBuffer,
-  }) => {
-    const videoRef = useRef<VideoRef>(null);
+const VideoPlayer = ({
+  mode = PlayerMode.AUTO,
+  uri,
+  style,
+  controls,
+  muted,
+  paused,
+  playInBackground,
+  rate,
+  repeat,
+  resizeMode,
+  volume,
+  onLoad,
+  onProgress,
+  onBuffer,
+  onError,
+  onEnd,
+}: VideoPlayerProps) => {
+  const videoRef = useRef<VideoRef>(null);
+  const reactNativeVideoHasError = useRef(false);
+  const supportedVideoFormats: Record<typeof Platform.OS, string[]> = {
+    ios: ['mp4', 'mp3', 'm3u8'],
+    android: ['mp4', 'mp3', 'm3u8'],
+    windows: [], // unsupported
+    macos: [], // unsupported
+    web: [], // unsupported
+  };
 
-    const isMp3OrMp4 = () => {
-      const lowerCasedUri = uri.toLowerCase();
-      return lowerCasedUri.endsWith('.mp3') || lowerCasedUri.endsWith('.mp4');
-    };
+  const isSupportedFormat = () => {
+    const lowerCasedUri = uri.toLowerCase();
+    const platform = Platform.OS;
+    const formats = supportedVideoFormats[platform];
+    return formats.some((format) => lowerCasedUri.endsWith(`.${format}`));
+  };
 
-    const shouldUseReactNativeVideoPlayer = () => {
-      switch (mode) {
-        case PlayerMode.AUTO:
-          return isMp3OrMp4();
-        case PlayerMode.VLC:
-          return false;
-        case PlayerMode.REACT_NATIVE_VIDEO:
-          return true;
-      }
-    };
+  const shouldUseReactNativeVideoPlayer = () => {
+    switch (mode) {
+      case PlayerMode.AUTO:
+        return isSupportedFormat() && !reactNativeVideoHasError.current;
+      case PlayerMode.VLC:
+        return false;
+      case PlayerMode.REACT_NATIVE_VIDEO:
+        return true;
+    }
+  };
 
-    return (
-      <>
-        <Text>
-          {shouldUseReactNativeVideoPlayer() ? 'ReactNativeVideo' : 'VLCPlayer'}
-        </Text>
-        {shouldUseReactNativeVideoPlayer() && (
-          <Video
-            source={{ uri }}
-            style={[styles.container, style]}
-            controls={controls}
-            muted={muted}
-            paused={paused}
-            playInBackground={playInBackground}
-            rate={rate}
-            repeat={repeat}
-            resizeMode={resizeMode}
-            volume={volume}
-            ref={videoRef}
-            onLoad={(event) => onLoad?.({ duration: event.duration })}
-            onProgress={(event) =>
-              onProgress?.({
-                duration: event.seekableDuration,
-                currentTime: event.currentTime,
-                position:
-                  event.seekableDuration > 0
-                    ? event.currentTime / event.seekableDuration
-                    : 0,
-                remainingTime: event.seekableDuration - event.currentTime,
-              })
+  return (
+    <>
+      {shouldUseReactNativeVideoPlayer() && (
+        <Video
+          source={{ uri }}
+          style={[styles.container, style]}
+          controls={controls}
+          muted={muted}
+          paused={paused}
+          playInBackground={playInBackground}
+          rate={rate}
+          repeat={repeat}
+          resizeMode={resizeMode}
+          volume={volume}
+          ref={videoRef}
+          onLoad={(event) => onLoad?.({ duration: event.duration })}
+          onProgress={(event) =>
+            onProgress?.({
+              duration: event.seekableDuration,
+              currentTime: event.currentTime,
+              position:
+                event.seekableDuration > 0
+                  ? event.currentTime / event.seekableDuration
+                  : 0,
+              remainingTime: event.seekableDuration - event.currentTime,
+            })
+          }
+          onBuffer={(event) => {
+            onBuffer?.({ isBuffering: event.isBuffering });
+          }}
+          onEnd={onEnd}
+          onError={(event) => {
+            switch (mode) {
+              case PlayerMode.AUTO:
+                reactNativeVideoHasError.current = true;
+                break;
+              case PlayerMode.REACT_NATIVE_VIDEO:
+                onError?.({ error: event.error.errorString });
+                break;
+              case PlayerMode.VLC:
+                break;
             }
-            onBuffer={(event) => {
-              onBuffer?.({ isBuffering: event.isBuffering });
-            }}
-            // onError={(error) => {
-            //   console.log(`Video onError: ${JSON.stringify(error)}`);
-            // }}
-          />
-        )}
-        {!shouldUseReactNativeVideoPlayer() && (
-          <VLCPlayer
-            source={{ uri }}
-            style={[styles.container, style]}
-            muted={muted}
-            paused={paused}
-            rate={rate}
-            repeat={repeat}
-            volume={volume}
-            onLoad={(event) => onLoad?.({ duration: event.duration / 1000 })}
-            onProgress={(event) =>
-              onProgress?.({
-                duration: event.duration / 1000,
-                currentTime: event.currentTime / 1000,
-                position: event.position,
-                remainingTime: (event.duration - event.currentTime) / 1000,
-              })
-            }
-            onBuffering={(event) => {
-              console.log(JSON.stringify(event));
-            }}
-
-            // onError={(props) => {
-            //   console.log(`VLCPlayer onError: ${props.target}`);
-            // }}
-          />
-        )}
-      </>
-    );
-  }
-);
+          }}
+        />
+      )}
+      {!shouldUseReactNativeVideoPlayer() && (
+        <VLCPlayer
+          source={{ uri }}
+          style={[styles.container, style]}
+          muted={muted}
+          paused={paused}
+          rate={rate}
+          repeat={repeat}
+          volume={volume}
+          onLoad={(event) => onLoad?.({ duration: event.duration / 1000 })}
+          onProgress={(event) =>
+            onProgress?.({
+              duration: event.duration / 1000,
+              currentTime: event.currentTime / 1000,
+              position: event.position,
+              remainingTime: (event.duration - event.currentTime) / 1000,
+            })
+          }
+          onEnd={onEnd}
+          onError={() => {
+            onError?.({ error: undefined });
+          }}
+        />
+      )}
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     width: '100%',
-    height: '50%',
+    height: '100%',
   },
 });
 
